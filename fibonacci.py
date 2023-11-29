b7411b3d4ee2dde99b21861622ede6df1b7d1775
Hursh Gupta <hurshgupta1@gmail.com>
2023-11-29 17:02:12 +0530

    spaces to dashes

--- fibonacci.py
+++ fibonacci.py
@@ -5,7 +5,7 @@ next_number = num2
 count = 1
 
 while count <= n:
-	print(next_number, end=" ")
+	print(next_number, end=" - ")
 	count += 1
 	num1, num2 = num2, next_number
 	next_number = num1 + num2
